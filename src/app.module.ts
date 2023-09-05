c44d8a08e04740066c1332179d678ad025206690
jhonathan55 <jfgutierrezsaico@gmail.com>
2023-09-05 20:07:34 -0300

    funciona nodemolules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModifyModuleService } from './services/modify-module.service';
 import { AutoImportService } from './services/auto-import.service';
+import { NombreDelModuloModule } from './nombre-del-modulo/nombre-del-modulo.module';
+import { VanessitaTeQuieroModule } from './vanessita-te-quiero/vanessita-te-quiero.module';
 @Module({
   imports: [
-    
+    NombreDelModuloModule,
+    VanessitaTeQuieroModule
   ],
   controllers: [
     AppController
   ],
   providers: [
-    AppService, 
-    ModifyModuleService, 
+    AppService,
+    ModifyModuleService,
     AutoImportService
   ],
 })
-export class AppModule {}
+export class AppModule { }
